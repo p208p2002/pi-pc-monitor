e2ec657583edb6f83ce5e747071e968ea849b143
unknown <p208p2002@gmail.com>
2019-03-25 14:56:52 +0800

    no message

--- tcp_client.py
+++ tcp_client.py
@@ -1,45 +1,28 @@
 import socket
 import sys
 import argparse
+import time
 from core import getBasicMsg
 
 
 host = 'localhost'
 
 def echo_client(port):
-    """ A simple echo client """
-    # Create a TCP/IP socket
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    # Connect the socket to the server
-    server_address = (host, port)
-    print ("Connecting to %s port %s" % server_address)
-    sock.connect(server_address)
 
-    # Send data
-    try:
-        # Send data
-        message = str(getBasicMsg())
-        print ("Sending %s" % message)
-        sock.sendall(message.encode('utf-8'))
-        # Look for the response
-        amount_received = 0
-        amount_expected = len(message)
-        while amount_received < amount_expected:
-            data = sock.recv(16)
-            amount_received += len(data)
-            print ("Received: %s" % data)
-    except socket.error as e:
-        print ("Socket error: %s" %str(e))
-    except Exception as e:
-        print ("Other exception: %s" %str(e))
-    finally:
-        print ("Closing connection to the server")
-        sock.close()
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_address = (host, port)
+    sock.connect(server_address)
+    sock.send(str("123").encode('utf-8'))
+    time.sleep(3)
+    sock.send(str("123").encode('utf-8'))
+
+
 
 if __name__ == '__main__':
     # getBasicMsg()
     parser = argparse.ArgumentParser(description='Socket Server Example')
-    parser.add_argument('--port', action="store", dest="port", type=int, required=True)
+    # parser.add_argument('--port', action="store", dest="port", type=int, required=True)
     given_args = parser.parse_args()
-    port = given_args.port
+    # port = given_args.port
+    port = 8080
     echo_client(port)
